5a7aa463c47ec0de67e9dbce76383feacdd162f7
meonmal <jongmin1034@naver.com>
2026-02-06 15:45:03 +0900

    과녁 오브젝트 생성 완료

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    // 오브젝트의 회전 속도
+    [SerializeField]
+    private float rotateSpeed = 50f;
+    // 오브젝트의 회전 방향
+    [SerializeField]
+    private Vector3 rotateAxis = Vector3.forward;
+
+    private void Update()
+    {
+        // 오브젝트의 z축을 rotateSpeed의 속도 만큼 움직인다.
+        transform.Rotate(rotateAxis, rotateSpeed * Time.deltaTime);
+    }
+}
